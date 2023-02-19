05a6d22d06d7b7539e63f4bbe75bc55916716411
amalbabu2024 <118371283+amalbabu2024@users.noreply.github.com>
2023-02-19 21:11:13 +0530

    Add files via upload

---
+++ exp6.py
@@ -0,0 +1,13 @@
+n=int(input("enter the limit"))
+print("enter the data")
+list=[]
+for i in range(0,n):
+    ele=input()
+    list.append(ele)
+print(list)
+count=0
+for i in list:
+    for j in i:
+        if j=='a':
+            count=count+1
+print("a:",count)
